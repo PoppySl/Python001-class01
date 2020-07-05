02786ebbddfefcb0b1ff74fc26dd4dd3d6c1af84
苏磊 <>
2020-07-05 23:56:51 +0800

    第二週作業

---
+++ week02/spiders/spiders/pipelines.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+import pandas
+
+
+class SpidersPipeline:
+    # def process_item(self, item, spider):
+    #
+    #     return item
+
+    def process_item(self, item, spider):
+        name = item['name']
+        link = item['link']
+        tag = item['tag']
+        time = item['time']
+        output = [f'name: {name.strip()} tag: {tag.strip()} time: {time.strip()} link: {link.strip()}']
+        # print("=============================================")
+        # print(output)
+        movie = pandas.DataFrame(data=output)
+        movie.to_csv('../movie2.csv', mode='a', index=False, encoding='utf8')
+        return item
---
+++ week02/spiders/spiders/spiders/maoyan.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from spiders.items import SpidersItem
+from scrapy.selector import Selector
+import scrapy
+import lxml
+
+
+class MaoyanSpider(scrapy.Spider):
+    name = 'maoyan'
+    allowed_domains = ['maoyan.com']
+    start_urls = ['https://maoyan.com/films?showType=3']
+
+    def parse(self, response):
+        item = SpidersItem()
+        item_list = Selector(response=response).xpath(
+            '//div[@class="movie-item-hover"]')
+        for items in item_list[:10]:
+            name = items.xpath('./a/div/div[1]/span[1]/text()').extract()[0]
+            tag = items.xpath(
+                './a/div/div[2]/text()').extract()[1].strip('\n')
+            time = items.xpath(
+                './a/div/div[4]/text()').extract()[1].strip('\n')
+            link = items.xpath('./a/@href').get()
+            item['name'] = name
+            item['link'] = "htttps://maoyan.com" + link
+            item['tag'] = tag
+            item['time'] = time
+            # print(item)
+            yield item
+
